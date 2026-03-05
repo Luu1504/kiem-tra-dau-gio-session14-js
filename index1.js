2de57f7781407cc08898cd901f5a10feaa30bb7a
luu1504 <hotsuper75@gmail.com>
2026-03-05 07:58:58 +0700

    lession14

---
+++ index1.js
@@ -0,0 +1,12 @@
+let email = document.getElementsByClassName("email-box");
+let text = document.getElementsByClassName("text-box");
+let form = document.getElementsByTagName("form");
+let button = document.getElementsByClassName("button-box");
+
+console.log(email);
+console.log(text);
+console.log(form);
+console.log(button);
+
+
+
